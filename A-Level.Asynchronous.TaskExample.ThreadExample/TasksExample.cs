798c7f469cbf934b05286e6694e3f6c9ea820765
vlad <vlad19972017@gmail.com>
2023-03-14 19:53:53 +0200

    Add main functions for example.

---
+++ A-Level.Asynchronous.TaskExample.ThreadExample/TasksExample.cs
@@ -0,0 +1,10 @@
+﻿namespace A_Level.Asynchronous.TaskExample.ThreadExample;
+
+internal class TasksExample
+{
+    public static void ThreadExample()
+    {
+        var task = new Task(() => { });
+        task.Start();
+    }
+}
---
+++ A-Level.Asynchronous.TaskExample.ThreadExample/ThreadsExample.cs
@@ -0,0 +1,10 @@
+﻿namespace A_Level.Asynchronous.TaskExample.ThreadExample;
+
+internal sealed class ThreadsExample
+{
+    public static void ThreadExample()
+    {
+        var thread = new Thread(() => { });
+        thread.Start();
+    }
+}
