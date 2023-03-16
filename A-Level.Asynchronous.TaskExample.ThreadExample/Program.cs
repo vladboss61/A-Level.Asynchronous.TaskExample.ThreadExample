17fdc9da8ff24fc95864de1e75705137efa42dd6
vlad <vlad19972017@gmail.com>
2023-03-16 10:46:45 +0200

    Async Await and threads.

--- A-Level.Asynchronous.TaskExample.ThreadExample/Program.cs
+++ A-Level.Asynchronous.TaskExample.ThreadExample/Program.cs
@@ -1,9 +1,53 @@
-﻿namespace A_Level.Asynchronous.TaskExample.ThreadExample;
+﻿using System.Diagnostics;
 
-internal class Program
+namespace A_Level.Asynchronous.TaskExample.ThreadExample;
+
+internal sealed class Program
 {
-    static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var result = await TasksExample.TaskCompletionSourceExampleAsync();
+        //await TasksExample.TaskExampleAsync();
+
+        //await TasksExample.TaskRun();
+        //await TasksExample.TaskContinueWithRunAsync();
+
+        //await TasksExample.DoubleAsyncExampleAsync();
+
+        //Thread.Sleep(10000);
+
+        //ThreadCounter.LockExample();
+
+        //var thread = new Thread(Something);
+        //thread.Start();
+
+        //for (int i = 0; i < 20; i++)
+        //{
+        //    Console.WriteLine($"Current Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        //    Console.WriteLine(i * 2);
+        //    Thread.Sleep(1000);
+        //}
+
+
+        //var processNode = Process.Start("node.exe");
+
+        //ThreadsExample.CurrentThread();
+        //ThreadsExample.ThreadExample();
+
+        //ThreadsExample.TimerExample();
+        //ThreadCounter.LockExample();
+        //TasksExample.DoubleAsyncExampleAsync();
+        //await TasksExample.ThreadExampleAsync();
+        Console.WriteLine("End of program.");
+    }
+
+    public static void Something()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"Created Thread Current Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine(i * 5);
+            Thread.Sleep(4000);
+        }
     }
 }
